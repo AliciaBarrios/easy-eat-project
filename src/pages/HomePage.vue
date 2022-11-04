26f919819467d4d6ff46d0ebb1583cf18775d664
Alicia Barrios <aliciabgo@gmail.com>
2022-11-04 21:41:12 +0100

    homePage done

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,90 @@
 <template>
     <q-page padding>
-        <h1>Hola Home Page</h1>
+        <h2>Hello *User*</h2>
+        <div class="flex column items-center">
+          <h4 
+          class="text-weight-light text-center" 
+          style="width:70%;"
+          >
+            Today we will leave your food to chance, click the button 
+            to see what the EasyEat holds for you...
+          </h4>
+          <q-btn :loading="loading" color="primary" @click="showRandomRecipe()" style="width: 150px">
+            Good Luck
+            <template v-slot:loading>
+              <q-spinner-hourglass class="on-left" />
+              Good luck...
+            </template>
+          </q-btn>
+        </div>
     </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import RecipeComponent from '../components/RecipeComponent.vue'
+  
+import { useRecipesStore } from '../stores/recipes'
+import { storeToRefs } from 'pinia'
+import { useQuasar } from 'quasar'
+  
+const recipeStore = useRecipesStore();
+const { recipes } = storeToRefs(recipeStore);
 
 export default defineComponent({
-  name: 'HomePage'
+  name: 'HomePage',
+
+  setup () {
+    const loading = ref(false)
+
+    const progress = ref(false)
+
+    const $q = useQuasar();
+
+    const fetchRecipes = async() => {
+      await recipeStore.fetchRecipes();
+    }
+
+    fetchRecipes();
+
+    const random = ref('');
+
+    function getRandomInt(length) {
+      return Math.floor(Math.random() * length);
+    }
+    
+    function showRandomRecipe () {
+      // we set loading state
+      loading.value = true;
+
+      random.value = getRandomInt(recipes.value.length);
+      const recipe = recipes.value[random.value];
+
+      // simulate a delay to create expectation
+      setTimeout(() => {
+        // we're done, we reset loading state
+        loading.value = false
+
+        $q.dialog({
+          component: RecipeComponent,
+          componentProps: {
+            recipe: recipe,
+          }
+        });   
+      }, 3000)
+    }
+
+    return {
+      loading,
+      progress,
+      showRandomRecipe,
+      fetchRecipes,
+      recipes,
+      getRandomInt,
+      random,
+    }
+  }
+
+
 });
 </script>
--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -1,13 +1,57 @@
 <template>
-    <q-page padding>
-        <h1>Hola Menu Page</h1>
-    </q-page>
+  <q-page padding>
+    <h2>Menu</h2>
+      <div>
+        <q-splitter
+          v-model="splitterModel"
+          style="height: 450px"
+        >
+
+          <template v-slot:before>
+            <div class="q-pa-md">
+              <q-date
+                v-model="date"
+                :events="events"
+                event-color="orange"
+              />
+            </div>
+          </template>
+
+          <template v-slot:after>
+            <q-tab-panels
+              v-model="date"
+              animated
+              transition-prev="jump-up"
+              transition-next="jump-up"
+              v-for="(event, index) in events" :key="index"
+            >
+              <q-tab-panel :name="event">
+                <div class="text-h4 q-mb-md">{{ event }}</div>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              </q-tab-panel>
+            </q-tab-panels>
+          </template>
+        </q-splitter>
+      </div>
+  </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { ref } from 'vue'
 
 export default defineComponent({
-  name: 'MenuPage'
+  name: 'MenuPage',
+
+
+  setup () {
+    return {
+      splitterModel: ref(50),
+      date: ('2022/11/05'),
+      events: [ '2022/11/05', '2022/11/06', '2022/11/07' ]
+    }
+  }
+
 });
 </script>
